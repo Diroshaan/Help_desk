24a32db306f49db2eeee558a1b5e2b4b50aa3a15
IT25103424 <IT25103424@my.sliit.lk>
2026-09-05 10:02:30 +0530

    Updated Ticket Repository

--- src/main/java/com/helpdesk/ticket/repository/TicketRepository.java
+++ src/main/java/com/helpdesk/ticket/repository/TicketRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
     List<Ticket> findByStudentId(Long studentId);
+
+    List<Ticket> findByCategory(String category);
 }
