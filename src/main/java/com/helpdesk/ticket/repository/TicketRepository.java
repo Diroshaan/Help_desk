302973af9cb1a69036830d9826e7495e45b4810a
IT25103424 <it25103424@my.sliit.lk>
2026-09-14 10:03:01 +0530

    Changes made to ticket repository file

--- src/main/java/com/helpdesk/ticket/repository/TicketRepository.java
+++ src/main/java/com/helpdesk/ticket/repository/TicketRepository.java
@@ -2,9 +2,10 @@ package com.helpdesk.ticket.repository;
 
 import com.helpdesk.ticket.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import java.util.List;
 
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
+public interface TicketRepository extends JpaRepository<Ticket, Long>, JpaSpecificationExecutor<Ticket> {
 
     List<Ticket> findByStudentId(Long studentId);
 
